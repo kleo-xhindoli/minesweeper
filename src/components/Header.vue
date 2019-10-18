da6a104f4eb781c2883901a3aa1215c5e373eb57
kxhindoli <kxhindoli@ritech.co>
2019-10-18 23:21:04 +0200

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="header">
+    <div class="box">
+      <div class="item score">{{ score }}</div>
+      <div class="item btn">
+        <div class="button">RST</div>
+      </div>
+      <div class="item time">{{ time }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Heder extends Vue {
+  @Prop() private score!: number;
+  @Prop() private time!: number;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.header {
+  display: flex;
+  justify-content: center;
+  font-weight: bold;
+
+  .box {
+    background: white;
+    display: flex;
+    border: 4px solid black;
+    justify-content: center;
+    width: 300px;
+    font-size: 32px;
+
+    .item:not(:last-child) {
+      border-right: 4px solid black;
+    }
+
+    .item {
+      flex: 1;
+    }
+
+    .button {
+      cursor: pointer;
+
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+
+    .icon {
+      margin-right: 10px;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,59 @@
 <template>
   <div class="home">
-    <Board :board="board" />
+    <Header :score="score" :time="timer" />
+    <div class="main">
+      <Board :board="board" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Board from "@/components/Board.vue";
+import Header from "@/components/Header.vue";
 import { createBoard } from "@/services/GameService.ts";
 
 export default {
   name: "home",
+  data() {
+    return {
+      timer: 0,
+      intervalId: null
+    };
+  },
   computed: {
     board() {
       return this.$store.state.board;
+    },
+    score() {
+      return this.$store.state.board.bombs - this.$store.getters.totalFlags;
+    },
+    isGameOver() {
+      return this.$store.state.lose || this.$store.getters.win;
+    },
+    totalRevealed() {
+      return this.$store.getters.totalRevealed;
     }
   },
   components: {
-    Board
+    Board,
+    Header
+  },
+  mounted() {
+    this.intervalId = setInterval(() => {
+      if (!this.isGameOver && this.totalRevealed > 0) this.timer++;
+    }, 1000);
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.home {
+.main {
   height: 100%;
   width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 20px;
 }
 </style>
